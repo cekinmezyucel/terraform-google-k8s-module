13efc58d82e686a3379105b9588044d1d74b5c1b
Yucel Cekinmez <cekinmezyucel@gmail.com>
2020-07-27 00:43:59 +0200

    first commit

---
+++ main.tf
@@ -0,0 +1,31 @@
+resource "google_container_cluster" "cluster" {
+  name                     = "cluster"
+  project                  = var.project_id
+  location                 = var.location
+  network                  = var.network_self_link
+  subnetwork               = var.subnetwork_self_link
+  remove_default_node_pool = true
+  enable_kubernetes_alpha  = false
+  initial_node_count       = var.initial_node_count
+}
+
+resource "google_container_node_pool" "node_pool" {
+  for_each = var.node_pools
+
+  name               = each.key
+  project            = var.project_id
+  cluster            = google_container_cluster.cluster.name
+  location           = var.location
+  initial_node_count = each.value["min_node_count"]
+
+  autoscaling {
+    min_node_count = each.value["min_node_count"]
+    max_node_count = each.value["max_node_count"]
+  }
+
+  node_config {
+    preemptible  = each.value["preemptible"]
+    machine_type = each.value["machine_type"]
+    image_type   = each.value["image_type"]
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,69 @@
+# Required Parameters
+variable "project_id" {
+  description = "Google Cloud Platform Project Id"
+  type        = string
+}
+
+# Optional Parameters
+variable "name" {
+  description = "Cluster Name"
+  type        = string
+  default     = "cluster"
+}
+
+variable "location" {
+  description = "Region or zone for cluster. We'll use zone europe-west4-b"
+  type        = string
+  default     = "europe-west4-b"
+}
+
+variable "network_self_link" {
+  description = "Network self link for project's network"
+  type        = string
+  default     = "default"
+}
+
+variable "subnetwork_self_link" {
+  description = "Sub network self link for project's network"
+  type        = string
+  default     = "default"
+}
+
+variable "initial_node_count" {
+  description = "Number of initial nodes to deploy"
+  type        = number
+  default     = 1
+}
+
+variable "node_pools" {
+  description = "Map of node pools; all parameters within the config objects are required (min_node_count, max_node_count, image_type, machine_type, preemptible, labels, tags, auto_repair, auto_upgrade)"
+  type = map(
+    object({
+      min_node_count    = number
+      max_node_count    = number
+      preemptible       = bool
+      machine_type      = string
+      image_type        = string
+      labels            = map(string)
+      tags              = set(string)
+      auto_repair       = bool
+      auto_upgrade      = bool
+      max_pods_per_node = number
+    })
+  )
+
+  default = {
+    "pool-alfa" = {
+      min_node_count    = 1
+      max_node_count    = 2
+      machine_type      = "n1-standard-1"
+      preemptible       = false
+      image_type        = "COS"
+      labels            = {}
+      tags              = []
+      auto_repair       = true
+      auto_upgrade      = true
+      max_pods_per_node = 110
+    }
+  }
+}
